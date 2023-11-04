06f776a63db9f2f588f9bca560a00dee23b52498
LeeJiSeop <jjiisub@naver.com>
2023-11-04 09:46:05 +0900

    ✨ feat: 유저 토큰 발행

---
+++ src/domain/user/user_router.py
@@ -0,0 +1,73 @@
+from datetime import timedelta, datetime
+from typing import Annotated
+import secrets
+
+from fastapi import APIRouter, Depends, HTTPException
+from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
+from jose import jwt
+from sqlalchemy.orm import Session
+from passlib.context import CryptContext
+
+from src.core.models import User
+from src.core.database import get_db
+from src.domain.user import user_schema
+from src.core.redis_config import get_redis
+
+ACCESS_TOKEN_EXPIRE_SECONDS = 60 * 10
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl='/user/login')
+
+router = APIRouter(
+    prefix="/user"
+)
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+def get_user(email: str, db: Session):
+    return db.query(User).filter(User.email==email).first()
+
+
+@router.post("/signup")
+def user_create(created_user: user_schema.CreateUser, db: Session = Depends(get_db)):
+    user = get_user(created_user.email, db)
+    if user:
+        raise ValueError('같은 이메일의 계정이 이미 존재합니다.')
+    _user = User(
+        email = created_user.email,
+        fullname = created_user.fullname,
+        password = pwd_context.hash(created_user.password1)
+    )
+    db.add(_user)
+    db.commit()
+    return _user
+
+
+@router.post("/login")
+def login(form: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+    user = get_user(form.username, db)
+    if not user or not pwd_context.verify(form.password, user.password):
+        raise ValueError('이메일 또는 비밀번호가 잘못되었습니다.')
+    
+    access_token = secrets.token_hex(32)
+    rd = get_redis()
+    rd.set(access_token, user.email)
+    rd.expire(access_token, ACCESS_TOKEN_EXPIRE_SECONDS)
+
+    return {
+        "access_token": access_token,
+        "token_type": "bearer",
+        "user_email": user.email
+    }
+
+# @router.get("/curr_user")
+def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
+    rd = get_redis()
+    user_email = rd.get(token)
+    return user_email
+
+@router.post("/logout")
+def logout(token: Annotated[str, Depends(oauth2_scheme)]):
+    rd = get_redis()
+    rd.delete(token)
+    return {"msg":"정상적으로 로그아웃되었습니다."}
