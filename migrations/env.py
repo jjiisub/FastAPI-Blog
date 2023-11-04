c124b7da541c9364195c2277214c37c498bcef1c
LeeJiSeop <jjiisub@naver.com>
2023-11-04 16:25:47 +0900

    🐛 fix: DB 연결 오류 해결

--- migrations/env.py
+++ migrations/env.py
@@ -2,10 +2,10 @@ from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
-import src.core.models as models
 
 from alembic import context
 
+from src.core import models
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
