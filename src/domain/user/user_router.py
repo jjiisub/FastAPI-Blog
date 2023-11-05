e74d94aa440f12d355f63f2f5b05d10e1904b5ff
LeeJiSeop <jjiisub@naver.com>
2023-11-05 12:08:45 +0900

    ✨ feat: HTTP error code

--- src/domain/user/user_router.py
+++ src/domain/user/user_router.py
@@ -1,7 +1,7 @@
 from typing import Annotated
 import secrets
 
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, status, HTTPException
 from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
 from sqlalchemy.orm import Session
 from passlib.context import CryptContext
@@ -10,9 +10,7 @@ from src.core.models import User
 from src.core.database import get_db
 from src.domain.user import user_schema
 from src.core.redis_config import get_redis
-from src.utils.config import Settings, get_settings
-
-# ACCESS_TOKEN_EXPIRE_SECONDS = 60 * 60 * 24
+from src.utils.config import Settings
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl='/user/login')
 
@@ -31,7 +29,7 @@ def get_user(email: str, db: Session):
 def user_create(created_user: user_schema.CreateUser, db: Session = Depends(get_db)):
     user = get_user(created_user.email, db)
     if user:
-        raise ValueError('같은 이메일의 계정이 이미 존재합니다.')
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="같은 이메일의 계정이 이미 존재합니다.")
     _user = User(
         email = created_user.email,
         fullname = created_user.fullname,
@@ -46,14 +44,11 @@ def user_create(created_user: user_schema.CreateUser, db: Session = Depends(get_
 def login(form: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
     user = get_user(form.username, db)
     if not user or not pwd_context.verify(form.password, user.password):
-        raise ValueError('이메일 또는 비밀번호가 잘못되었습니다.')
-    
-    settings = get_settings()
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='이메일 또는 비밀번호가 잘못되었습니다.')
     access_token = secrets.token_hex(32)
     rd = get_redis()
     rd.set(access_token, user.id)
-    rd.expire(access_token, settings.ACCESS_TOKEN_EXPIRE_SECONDS)
-
+    rd.expire(access_token, Settings().ACCESS_TOKEN_EXPIRE_SECONDS)
     return {
         "access_token": access_token,
         "token_type": "bearer",
@@ -63,8 +58,10 @@ def login(form: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get
 # @router.get("/curr_user")
 def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
     rd = get_redis()
-    user_id = int(rd.get(token))
-    return user_id
+    user_id = rd.get(token)
+    if not user_id:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='로그인이 필요합니다.')
+    return int(user_id)
 
 @router.post("/logout")
 def logout(token: Annotated[str, Depends(oauth2_scheme)]):
--- src/domain/user/user_schema.py
+++ src/domain/user/user_schema.py
@@ -1,3 +1,4 @@
+from fastapi import status, HTTPException
 from pydantic import BaseModel, validator, EmailStr
 
 class CreateUser(BaseModel):
@@ -9,16 +10,10 @@ class CreateUser(BaseModel):
     @validator('email','fullname','password1','password2')
     def not_empty(cls, v):
         if not v or not v.strip():
-            raise ValueError("빈 칸을 모두 채워주세요")
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="빈 칸을 모두 채워주세요")
         return v
     
     @validator('password2')
-    def password_chk(cls, v, values):
+    def password_check(cls, v, values):
         if v != values['password1']:
-            raise ValueError('입력한 비밀번호가 비밀번호가 서로 다릅니다')
-
-
-class Token(BaseModel):
-    access_token: str
-    token_type: str
-    email: str
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="입력된 비밀번호가 서로 다릅니다.")
