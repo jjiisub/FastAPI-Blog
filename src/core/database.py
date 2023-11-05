ece73c1b66cfbfbef0b6b44c0857875aed1ab871
LeeJiSeop <jjiisub@naver.com>
2023-11-05 12:07:36 +0900

    💬 comment: get_db 주석 추가

--- src/core/database.py
+++ src/core/database.py
@@ -2,18 +2,16 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-from src.utils.config import get_settings
+from src.utils.config import Settings
 
-settings = get_settings()
-# SQLALCHEMY_DATABASE_URL = "postgresql://postgres:elice@localhost/elice_db"
-
-engine = create_engine(settings.SQLALCHEMY_DATABASE_URL)
+engine = create_engine(Settings().SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
 
 
 def get_db():
+    '''DB 세션 관리 함수'''
     db = SessionLocal()
     try:
         yield db
