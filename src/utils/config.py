44fb9a49b24dc31bb8aa043d2e2c25bee9ae3c69
LeeJiSeop <jjiisub@naver.com>
2023-11-05 14:07:11 +0900

    💬 comment: 주석 추가

--- src/utils/config.py
+++ src/utils/config.py
@@ -2,12 +2,16 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
+    '''
+    .env 파일 로드
+
+        Attributes:
+            ACCESS_TOKEN_EXPIRE_SECONDS (int): 로그인 세션 유지 기간 (redis 만료)
+            SQLALCHEMY_DATABASE_URL (str): PostgreSQL DB 연결 주소
+            PAGE_SIZE (str): pagination 단위
+    '''
     ACCESS_TOKEN_EXPIRE_SECONDS: int = 0
     SQLALCHEMY_DATABASE_URL: str = ""
-    PAGE_SIZE: int = 0
+    PAGE_SIZE: int = 1
 
     model_config = SettingsConfigDict(env_file=".env")
-
-
-def get_settings():
-    return Settings()
